dae7703d381104e554fd482b6838630e526599e3
MizzUNCA <thesoloflame@gmail.com>
2023-04-12 23:09:52 -0400

    hw6completed

--- final-project/mishko/mishko/main.js
+++ final-project/mishko/mishko/main.js
@@ -1,49 +1,50 @@
 async function getCharacter() {
-    const response = await fetch('https://api.jikan.moe/v4/characters?q=naruto');
+    const response = await fetch('https://api.jikan.moe/v4/anime?q=danganronpa');
     const obj = await response.json();
     console.log(obj);
     for (let i = 0; i < obj.data.length; i++) {
         const item = obj.data[i];
         const template = `
             <section>
-                <img src="${item.images.jpg.image_url}" />
-                <p>${item.name}</p>
+                <img src="${item.anime.images.jpg.large_image-url}" />
+                <p>${item.mal_id}</p>
             </section>`;
         document.querySelector('main').insertAdjacentHTML('beforeend', template);
     }
 }
 
-// async function otherTest() {
-//     const response = await fetch('https://api.jikan.moe/v4/characters/17/manga');
-//     const obj = await response.json();
-//     console.log(obj);
-//     for (let i = 0; i < obj.data.length; i++) {
-//         const item = obj.data[i];
-//         const template = `
-//             <section>
-//                 <img src="${item.manga.images.jpg.image_url}" />
-//                 <p>${item.manga.title}</p>
-//             </section>`;
-//         document.querySelector('main').insertAdjacentHTML('beforeend', template);
-//     }
-// }
+async function otherTest() {
+    const response = await fetch('https://api.jikan.moe/v4/characters/17/manga');
+    const obj = await response.json();
+    console.log(obj);
+    for (let i = 0; i < obj.data.length; i++) {
+        const item = obj.data[i];
+        const template = `
+            <section>
+                <img src="${item.manga.images.jpg.image_url}" />
+                <p>${item.manga.title}</p>
+                <p>${item.volumes} </p>
+            </section>`;
+        document.querySelector('main').insertAdjacentHTML('beforeend', template);
+    }
+}
 
 async function userTest() {
-    const response = await fetch('https://api.jikan.moe/v4/characters?qgoku');
+    const response = await fetch('https://api.jikan.moe/v4/anime?q=danganronpa');
     const obj = await response.json();
     console.log(obj);
     for (let i = 0; i < obj.data.length; i++) {
         const item = obj.data[i];
         const template = `
             <section>
-                <img src="${item.images.jpg.image_url}" />
+                <img src="${item.jpg.image_url}" />
             </section>`;
         document.querySelector('main').insertAdjacentHTML('beforeend', template);
     }
 }
 
 
-//etCharacter();
-//otherTest();
+getCharacter();
+otherTest();
 userTest();
 
--- final-project/project.js
+++ final-project/project.js
@@ -1,4 +1,16 @@
-var jikanjs = require("@mateoaranda/jikanjs");
+async function getCharacter() {
+    const response = await fetch('https://api.jikan.moe/v4/anime?q=dragonball');
+    const obj = await response.json();
+    console.log(obj);
+    for (let i = 0; i < obj.data.length; i++) {
+        const item = obj.data[i];
+        const template = `
+            <section>
+                <img src="${item.images.jpg.large_image-url}" />
+                <p>${item.name}</p>
+            </section>`;
+        document.querySelector('.item.homework').insertAdjacentHTML('beforeend', template);
+    }
+}
 
-jikanjs.loadAnime(51535);
-
+getCharacter();
